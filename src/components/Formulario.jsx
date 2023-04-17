6a5989eddc7164b852cdf7e7725bb02e7b968f85
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-17 12:39:51 -0400

    Finalizado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,69 @@
+import { marcas, years, planes } from "../constants";
+
 const Formulario = () => {
     return (
         <form>
-            <div class="mb-6">
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Select your country
+            <div className="mb-3">
+                <label
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    htmlFor="marca"
+                >
+                    Marca
                 </label>
-                <select class="border text-sm rounded-lg  block w-full p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150">
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>France</option>
-                    <option>Germany</option>
+                <select
+                    id="marca"
+                    className="border text-sm rounded-lg  block w-full p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150"
+                >
+                    <option value="">--Seleccionar--</option>
+                    {marcas.map((marca) => (
+                        <option value={marca.id} key={marca.id}>
+                            {marca.nombre}
+                        </option>
+                    ))}
                 </select>
             </div>
-            <div class="mb-6">
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Select your country
+            <div className="mb-3">
+                <label
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    htmlFor="year"
+                >
+                    Año
                 </label>
-                <select class="border text-sm rounded-lg  block w-full p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150">
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>France</option>
-                    <option>Germany</option>
+                <select
+                    id="year"
+                    className="border text-sm rounded-lg  block w-full p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150"
+                >
+                    <option value="">--Seleccionar--</option>
+                    {years.map((year) => (
+                        <option value={year} key={year}>
+                            {year}
+                        </option>
+                    ))}
                 </select>
+            </div>
+            <div>
+                <label>Plan:</label>
+                {planes.map((plan) => (
+                    <div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-3">
+                        <input
+                            id={plan.id}
+                            type="radio"
+                            value={plan.id}
+                            name="plan"
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                            htmlFor={plan.id}
+                            class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            {plan.nombre}
+                        </label>
+                    </div>
+                ))}
             </div>
             <button
                 type="Submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Cotizar
             </button>

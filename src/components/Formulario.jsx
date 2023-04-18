0643e1f1043a3a9ff291b2760a0aa116038065e3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-18 00:30:30 -0400

    Fix key

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -44,17 +44,20 @@ const Formulario = () => {
             <div>
                 <label>Plan:</label>
                 {planes.map((plan) => (
-                    <div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-3">
+                    <div
+                        className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-3"
+                        key={plan.id}
+                    >
                         <input
                             id={plan.id}
                             type="radio"
                             value={plan.id}
                             name="plan"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                         />
                         <label
                             htmlFor={plan.id}
-                            class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
                             {plan.nombre}
                         </label>

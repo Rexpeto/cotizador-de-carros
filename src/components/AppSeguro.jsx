8bbf1317c285cb08b469d65bf84a38cac3febc7b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-18 00:30:02 -0400

    Actualizado

--- src/components/AppSeguro.jsx
+++ src/components/AppSeguro.jsx
@@ -6,7 +6,7 @@ const AppSeguro = () => {
             <header className="my-6 font-bold text-2xl">
                 <h1>Cotizador de Carros</h1>
             </header>
-            <main className="md:w-2/3 lg:w-2/4 p-6 border rounded-lg shadow bg-gray-800/80 border-gray-700">
+            <main className="md:w-2/3 lg:w-2/4 p-6 border rounded-lg shadow bg-gray-800/80 border-gray-700 w-full">
                 <Formulario />
             </main>
         </>

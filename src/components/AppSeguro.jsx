c81236f1d4b2749278e2d47700abd3a95faac306
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-19 03:03:29 -0400

    Mostrando resultado

--- src/components/AppSeguro.jsx
+++ src/components/AppSeguro.jsx
@@ -1,6 +1,10 @@
+import useCotizador from "../hooks/useCotizador";
 import Formulario from "./Formulario";
+import Resultado from "./Resultado";
 
 const AppSeguro = () => {
+    const { resultado, cargando } = useCotizador();
+
     return (
         <>
             <header className="my-6 font-bold text-2xl">
@@ -8,6 +12,13 @@ const AppSeguro = () => {
             </header>
             <main className="md:w-2/3 lg:w-2/4 p-6 border rounded-lg shadow bg-gray-800/80 border-gray-700 w-full">
                 <Formulario />
+                <div className="flex flex-col items-center justify-center">
+                    {cargando ? (
+                        <span className="loader"></span>
+                    ) : (
+                        <Resultado resultado={resultado} />
+                    )}
+                </div>
             </main>
         </>
     );
